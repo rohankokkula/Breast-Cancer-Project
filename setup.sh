84be6f8653b5ae8452e0bb1aaa11d01464698c55
dannylee1020 <dannylee1020@gmail.com>
2020-07-06 09:42:44 -0700

    commit

--- setup.sh
+++ setup.sh
@@ -1,15 +1,23 @@
 mkdir -p ~/.streamlit/
 
 
-echo “\
-[general]\n\
-email = \”dannylee1020@gmail.com\”\n\
-“ > ~/.streamlit/credentials.toml
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false
+" > ~/.streamlit/config.toml
 
 
-echo “\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
-“ > ~/.streamlit/config.toml
+
+# echo “\
+# [general]\n\
+# email = \”dannylee1020@gmail.com\”\n\
+# “ > ~/.streamlit/credentials.toml
+
+
+# echo “\
+# [server]\n\
+# headless = true\n\
+# enableCORS=false\n\
+# port = $PORT\n\
+# “ > ~/.streamlit/config.toml
